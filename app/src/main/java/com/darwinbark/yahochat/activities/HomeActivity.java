6452e4aa4234e504238da85f6b9ec36deacb2be9
shubhcode12 <shubhmahajan1234@gmail.com>
2021-08-26 23:24:38 -0700

    fixed databinding

--- app/src/main/java/com/darwinbark/yahochat/activities/HomeActivity.java
+++ app/src/main/java/com/darwinbark/yahochat/activities/HomeActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -38,9 +42,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+        if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, 101);
+        }
 
-        setListeners();
+
+
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -73,6 +81,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
         drawerLayout.setDrawerListener(drawerToggle);
         drawerToggle.syncState();
+
+        setListeners();
+        initializeBottomBar();
 
     }
 
@@ -222,6 +233,24 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         transaction.replace(R.id.coordinator_layout_fragment_container, fragment,
                 fragment.getClass().getSimpleName());
         transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
+        }
+
+        if (mBinding.imageViewGenerateActive.getVisibility() != View.VISIBLE) {
+
+            clickOnHome();
+        } else {
+            finish();
+            finishAffinity();
+
+        }
+
     }
 
 
